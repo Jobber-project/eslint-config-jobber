e7166c90f9370512e5f8fda289b9f3ac3511625e
Jesper Johansson <johansson.jesper@outlook.com>
2021-06-01 21:29:20 +0200

    Add jest support

--- index.js
+++ index.js
@@ -31,5 +31,10 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       plugins: ['prettier', 'react-hooks', '@typescript-eslint'],
     },
+    {
+      files: ['*.test.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['prettier', 'react-hooks', '@typescript-eslint', 'jest'],
+    },
   ],
 }
