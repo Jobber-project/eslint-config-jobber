1a423a391ec0afdf84d2ba9c813e44699989418e
Jesper Johansson <johansson.jesper@outlook.com>
2021-08-22 11:39:13 +0200

    fix: import extensions

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ module.exports = {
     'react/require-default-props': 'off',
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
   },
   overrides: [
     {
@@ -43,6 +53,10 @@ module.exports = {
     },
   ],
   settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx']
