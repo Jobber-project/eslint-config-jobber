6dde55efa7c73381c4ac0b927e84f4cc2f330ff6
Jesper Johansson <johansson.jesper@outlook.com>
2021-08-22 12:14:34 +0200

    feat: add no-unused-vars rule

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ module.exports = {
       }
     ],
     'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error']
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
   },
   overrides: [
     {
